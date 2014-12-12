cdf1be02446ed427ae6ce7d0a3191b00be802645
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-11 22:31:30 -0800

    added ubio_search fxn

---
+++ pytaxize/ubio.py
@@ -0,0 +1,61 @@
+from lxml import etree
+import pandas as pd
+
+def ubio_search(searchName = None, searchAuth = None, searchYear = None, 
+    order = None, sci = None, vern = None, keyCode = None):
+    '''
+    ubio_search returns NameBankIDs that match search terms
+
+    :param searchName: (string) - term to search within name string
+    :param searchAuth: (string) - term to search within name authorship
+    :param searchYear: (string) - term to search within name year
+    :param order: (string) - (name or namebankID) field by which the results will 
+    be sorted (default is namebankID)
+    :param sci: (int) - (sci, vern, or all) type of results to be returned 
+    (default is all)
+    :param vern: (int) - (limit 1000) maximum number of results to be returned 
+    (default is 1000)
+    :param keyCode: Your uBio API key; loads from .Rprofile. If you don't have 
+       one, obtain one at http://www.ubio.org/index.php?pagename=form.
+    :param callopts: Parameters passed on to httr::GET call.
+
+    Usage
+    # A basic example
+    >>> import pytaxize
+    >>>
+    >>> pytaxize.ubio_search(searchName = 'elephant', sci = 1, vern = 0)
+    >>> pytaxize.ubio_search(searchName = 'Astragalus aduncus', sci = 1, vern = 0)
+    '''
+    url = "http://www.ubio.org/webservices/service.php"
+    ubioApiKey = "b052625da5f330e334471f8efe725c07bf4630a6"
+    payload = {'function': 'namebank_search', 'searchName': searchName, 
+               'searchAuth': searchAuth, 'searchYear': searchYear, 'order': order,
+               'sci': sci, 'vern': vern, 'keyCode': ubioApiKey}
+    out = requests.get(url, params = payload)
+    out.raise_for_status()
+    xmlparser = etree.XMLParser()
+    tt = etree.fromstring(out.content, xmlparser)
+    nodes = tt.xpath('//value')
+
+    outlist = []
+    for i in range(len(nodes)):
+        tt_ = nodes[i].getchildren()
+        outlist.append([x.text for x in tt_[:8]])
+    df = pd.DataFrame(outlist, columns=['namebankID','nameString','fullNameString','packageID','packageName','basionymunit','rankID','rankName'])
+    return df
+    # tt = content(tmp)
+    # toget = c("namebankID", "nameString", "fullNameString", "packageID", 
+    #                    "packageName", "basionymUnit", "rankID", "rankName")
+    # temp2 = lapply(toget, function(x) sapply(xpathApply(tt, paste("//", x, sep="")), xmlValue))
+    # temp2[2:3] = sapply(temp2[2:3], base64Decode)
+    # out = data.frame(do.call(cbind, temp2))
+    # names(out) = c("namebankid", "namestring", "fullnamestring", "packageid", 
+    #                               "packagename", "basionymunit", "rankid", "rankname")
+    
+
+# searchAuth = None
+# searchYear = None
+# order = None
+# sci = None
+# vern = None
+# keyCode = None
