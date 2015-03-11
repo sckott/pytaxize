5055b31ccb5969d036ff6b3240b3dd4a10835ef5
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-11 11:13:14 -0700

    tidied gnr_resolve a bit, changed parsing and join > 1 names with pipe, #12

--- pytaxize/gnr.py
+++ pytaxize/gnr.py
@@ -69,16 +69,22 @@ def gnr_resolve(names='Homo sapiens', source=None, format='json', resolve_once='
     >>> import pytaxize
     >>> pytaxize.gnr_resolve('Helianthus annus')
     [{u'classification_path': u'', u'data_source_title': u'EOL', u'match_type': 1, u'score': 0.988, u'url': u'http://eol.org/pages/468106/names/synonyms', u'classification_path_ranks': u'', u'name_string': u'Helianthus annus', u'prescore': u'3|0|0', u'canonical_form': u'Helianthus annus', u'classification_path_ids': u'', u'local_id': u'468106', u'data_source_id': 12, u'taxon_id': u's_5106367', u'gni_uuid': u'f5674e32-00cc-57e3-b632-6a0b89fa4df4'}, {u'classification_path': u'|Helianthus annus', u'data_source_title': u'uBio NameBank', u'match_type': 1, u'score': 0.988, u'url': u'http://www.ubio.org/browser/details.php?namebankID=10130157', u'classification_path_ranks': u'kingdom|', u'name_string': u'Helianthus annus', u'global_id': u'urn:lsid:ubio.org:namebank:10130157', u'prescore': u'3|0|0', u'canonical_form': u'Helianthus annus', u'classification_path_ids': u'', u'local_id': u'urn:lsid:ubio.org:namebank:10130157', u'data_source_id': 169, u'taxon_id': u'102910884', u'gni_uuid': u'f5674e32-00cc-57e3-b632-6a0b89fa4df4'}, {u'classification_path': u'', u'data_source_title': u'EOL', u'match_type': 2, u'score': 0.988, u'url': u'http://eol.org/pages/468106', u'classification_path_ranks': u'', u'name_string': u'Helianthus annus L.', u'prescore': u'3|0|0', u'canonical_form': u'Helianthus annus', u'classification_path_ids': u'', u'local_id': u'468106', u'data_source_id': 12, u'taxon_id': u'20584982', u'gni_uuid': u'e757b3c1-421f-5bb9-a27f-d56259baaf3d'}]
+    >>> pytaxize.gnr_resolve(['Helianthus annus','Poa annua'])
     '''
     url = 'http://resolver.globalnames.org/name_resolvers'
-    payload = {'names': names, 'data_source_ids': source, 'format': format, 'resolve_once': resolve_once,
-               'with_context': with_context, 'best_match_only': best_match_only, 'header_only': header_only,
-               'preferred_data_sources': preferred_data_sources}
+    if(names.__class__.__name__ == 'list'):
+        names = "|".join(names)
+    else:
+        pass
+    payload = {'names': names, 'data_source_ids': source, 'format': format,
+                'resolve_once': resolve_once, 'with_context': with_context,
+                'best_match_only': best_match_only, 'header_only': header_only,
+                'preferred_data_sources': preferred_data_sources}
     out = requests.get(url, params = payload)
     out.raise_for_status()
     result_json = out.json()
     try:
-        data = result_json['data'][0]['results']
+        data = [ each_result['results'] for each_result in result_json['data'] ]
         return data
     except (KeyError, IndexError):
         raise NoResultException("GNR didn't return a result (names: %s)" % names)
