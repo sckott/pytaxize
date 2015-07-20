0e14bb34afca11cf5d4eb3b3e909628ec12e43e8
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-19 23:39:02 -0700

    require current versions of dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='pytaxize',
-	version='0.2.99',
+	version='0.3.900',
 	description='Taxonomic toolbelt for Python',
   author='Scott Chamberlain',
   author_email='myrmecocystus@gmail.com',
   url='https://github.com/sckott/pytaxize',
   packages=['pytaxize'],
-  install_requires=['requests>2.0',
-                    'pandas>0.1',
+  install_requires=['requests>2.7.0',
+                    'pandas>0.15.2',
                     'lxml'],
   data_files=[('pytaxize/data', ['data/apg_orders.csv', 'data/apg_families.csv', 'data/plantGenusNames.csv', 'data/plantNames.csv', 'data/rank_ref.csv'] )],
 )
