9d0b16052c76c92f94ff8f463a9d385b5b6a8deb
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-17 09:38:57 -0800

    trying fix for readthedocs, add pandas to mock in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['lxml']
+MOCK_MODULES = ['lxml', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
