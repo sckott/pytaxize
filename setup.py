fea3f6005e19655f56e4abf5cb449283e05aedf3
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-10 10:19:26 -0700

    install vcrpy and vcrpy-unittest

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   packages         = find_packages(exclude=['test-*']),
   install_requires=['pandas','requests>=2.7.0','lxml'],
   extras_require={
-    'test': ['pytest', 'vcrpy', 'vcrpy-unittest'],
+    'test': ['vcrpy', 'vcrpy-unittest'],
   },
   data_files=[('pytaxize/data', ['data/apg_orders.csv', 'data/apg_families.csv',
     'data/plantGenusNames.csv', 'data/plantNames.csv', 'data/rank_ref.csv'] )],
