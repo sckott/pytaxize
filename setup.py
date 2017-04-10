5d896dfb95bd273f65e2d88766cd8a7a59187604
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-09 20:29:34 -0700

    attempt travis file fix

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   packages         = find_packages(exclude=['test-*']),
   install_requires=['pandas','requests>=2.7.0','lxml'],
   extras_require={
-       'test': ['vcr_unittest'],
+       'test': ['vcrpy-unittest'],
   },
   data_files=[('pytaxize/data', ['data/apg_orders.csv', 'data/apg_families.csv',
     'data/plantGenusNames.csv', 'data/plantNames.csv', 'data/rank_ref.csv'] )],
