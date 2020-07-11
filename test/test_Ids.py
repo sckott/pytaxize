add5e819997ef4c4874000a370422999d51311f5
Daniel-Davies <dd16785@bristol.ac.uk>
2020-07-11 19:15:59 +0100

    uncommented valid test

--- test/test_Ids.py
+++ test/test_Ids.py
@@ -7,20 +7,20 @@ import pytest
 
 
 class IdsTest(unittest.TestCase):
-    # @vcr.use_cassette("test/vcr_cassettes/ids_ncbi.yml")
-    # @pytest.mark.skipif(
-    #     "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-    #     reason="Skipping this test on Travis CI.",
-    # )
-    # def test_ids_ncbi(self):
-    #     "Ids: ncbi"
-    #     x = Ids("Poa annua")
-    #     assert type(x) == Ids
-    #     assert x.name == ["Poa annua"]
-    #     assert len(x.ids) == 0
-    #     x.ncbi()
-    #     assert len(x.ids) > 0
-    #     assert x.db_ids == "ncbi"
+    @vcr.use_cassette("test/vcr_cassettes/ids_ncbi.yml")
+    @pytest.mark.skipif(
+        "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+        reason="Skipping this test on Travis CI.",
+    )
+    def test_ids_ncbi(self):
+        "Ids: ncbi"
+        x = Ids("Poa annua")
+        assert type(x) == Ids
+        assert x.name == ["Poa annua"]
+        assert len(x.ids) == 0
+        x.ncbi()
+        assert len(x.ids) > 0
+        assert x.db_ids == "ncbi"
     
     @vcr.use_cassette("test/vcr_cassettes/ids_gbif_1.yml")
     def test_ids_gbif_single_name(self):
